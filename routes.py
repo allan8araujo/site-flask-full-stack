14f550ff2ec4d9695c97b65ad26982554abb3ae2
Allan Araújo <77680596+allan8araujo@users.noreply.github.com>
2021-05-22 23:47:14 -0300

    updating the FAQ page
    adding more info about the High elo job

---
+++ routes.py
@@ -0,0 +1,16 @@
+from aplicativo import app
+from flask import render_template,request,url_for
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/calc')
+def calc():
+    return render_template('calc.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
